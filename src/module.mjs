a56eb19a210587f9c9267a78ca6024ae16b50e0a
FurtherV <alex.howel@web.de>
2025-06-03 12:40:12 +0200

    init

---
+++ src/module.mjs
@@ -0,0 +1,23 @@
+import "./styles/style.less";
+
+import { MODULE_TITLE } from "./scripts/constants.mjs";
+import { log } from "./scripts/utils.mjs";
+import * as hooks from "./scripts/hooks/_module.mjs";
+
+function onInit() {
+  log(`Initializing ${MODULE_TITLE}.`);
+
+  hooks.initInjectConfig();
+
+  hooks.initExpirationWorldTime();
+
+  hooks.initExpirationCombatStart();
+  hooks.initExpirationCombatEnd();
+  hooks.initExpirationTurnChange();
+
+  if (game.system.id === "dnd5e") {
+    hooks.initDND5eRest();
+  }
+}
+
+Hooks.on("init", onInit);
---
+++ src/scripts/hooks/expiration-dnd5e-rest.mjs
@@ -0,0 +1,33 @@
+import { EXPIRATION_TYPES } from "../constants.mjs";
+import { effectShouldExpireOn, expireEffect, isEffectTemporary } from "../utils.mjs";
+
+export default function init() {
+  Hooks.on("dnd5e.restCompleted", onRestCompleted);
+}
+
+function onRestCompleted(actor, result, config) {
+  console.debug({ actor, result, config });
+
+  const isShortRest = !result.longRest;
+  const isLongRest = result.longRest;
+  const isNewDay = result.newDay;
+
+  const effects = actor.appliedEffects;
+  for (const effect of effects) {
+    if (!isEffectTemporary(effect)) continue;
+
+    if (effectShouldExpireOn(effect, EXPIRATION_TYPES.SYSTEM.DND5E.SHORT_REST) && isShortRest) {
+      expireEffect(effect, EXPIRATION_TYPES.SYSTEM.DND5E.SHORT_REST);
+      continue;
+    }
+
+    if (effectShouldExpireOn(effect, EXPIRATION_TYPES.SYSTEM.DND5E.LONG_REST) && isLongRest) {
+      expireEffect(effect, EXPIRATION_TYPES.SYSTEM.DND5E.LONG_REST);
+      continue;
+    }
+
+    if (effectShouldExpireOn(effect, EXPIRATION_TYPES.SYSTEM.DND5E.NEW_DAY) && isNewDay) {
+      expireEffect(effect, EXPIRATION_TYPES.SYSTEM.DND5E.NEW_DAY);
+    }
+  }
+}
